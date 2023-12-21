ee8908239ff1c25f7a38539bddd710ba445fbee9
jesusqc <jesusquirocampos@gmail.com>
2023-12-21 15:30:33 +0100

    Remove debug

--- SecretLabDependenciesBuilder/Program.cs
+++ SecretLabDependenciesBuilder/Program.cs
@@ -1,9 +1,5 @@
 ﻿// All in one - Server Downloader and Publicizer.
 
-#if DEBUG
-Environment.CurrentDirectory = @"C:\Users\jesus\Documents\_Coding\SecretLabDependencyBuilder";
-#endif
-
 Console.WriteLine("""
                   ███████████████████████████████████████
                   █─▄▄▄▄█▄─▄█████▄─▄▄▀█▄─▄▄─█▄─▄▄─█─▄▄▄▄█
